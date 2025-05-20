22e80f775691ce4ace404e0e7615c556ac60dca6
ramon-gorge <44533001+ramon-gorge@users.noreply.github.com>
2025-05-20 13:33:18 -0300

    add project id

--- cypress/cypress.config.js
+++ cypress/cypress.config.js
@@ -5,6 +5,7 @@ module.exports = defineConfig({
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
+    projectId: "dkd6wo",
     //video: true,
     specPattern: ["cypress/e2e/**/*.cy.{js,jsx,ts,tsx}", "e2e/**/*.cy.{js,jsx,ts,tsx}"], // Add this line
   },
